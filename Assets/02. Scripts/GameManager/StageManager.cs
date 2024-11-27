271a862ac6941c32b9c3fcda4e6560240f5fd839
Joo-Sung-Hwan <96138046+Joo-Sung-Hwan@users.noreply.github.com>
2024-11-27 16:51:09 +0900

    test

--- Assets/02. Scripts/GameManager/StageManager.cs
+++ Assets/02. Scripts/GameManager/StageManager.cs
@@ -2,8 +2,9 @@ using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class StageManager : Singleton<StageManager>
+public class StageManager : MonoBehaviourPunCallbacks
 {
     public Stage CurrentStage {  get; private set; }
 
@@ -11,7 +12,8 @@ public class StageManager : Singleton<StageManager>
 
     public void CreateStage(StageDetailsSO stageDetails) // 스테이지 생성
     {
-        Instantiate(stageDetails.stagePrefab, this.transform); // Instantiate에서 this.transform 이므로 자식으로 생성됨
+        PhotonNetwork.Instantiate("Stage1", Vector2.zero, Quaternion.identity);
+        //Instantiate(stageDetails.stagePrefab, this.transform); // Instantiate에서 this.transform 이므로 자식으로 생성됨
         Stage instantiatedStage = GetComponentInChildren<Stage>(); // 생성된 자식오브젝트에서 Stage 컴포넌트 가져오기
         instantiatedStage.InitializedStage(stageDetails); // 생성된 스테이지 초기화
 
